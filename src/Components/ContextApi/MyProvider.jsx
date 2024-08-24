77b5d1af4a1d0e056a1c34b5c6f0514d69d1a8d2
nachikett01 <nachikettekade01@gmail.com>
2024-08-24 19:11:14 +0530

    fire1

--- src/Components/ContextApi/MyProvider.jsx
+++ src/Components/ContextApi/MyProvider.jsx
@@ -1,8 +1,8 @@
 import MyContext from "./MyContext";
 
 const MyProvider = ({ children }) => {
-  const api = "http://127.0.0.1:8000/api3/simulation"
-  // const api = "https://api.cyberbriefs.org/api3/simulation";
+  // const api = "http://127.0.0.1:8000/api3/simulation"
+  const api = "https://api.cyberbriefs.org/api3/simulation";
 
   return (
     <MyContext.Provider
